5e0943891969e483023f834fc0088d52a5dece85
Oleksii_Omelchenko <53217117+Omal1k@users.noreply.github.com>
2024-11-18 11:01:33 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
-import { createGlobalStyle } from "styled-components"; # :)
+import { createGlobalStyle } from "styled-components"; // :)
 
 const Global = createGlobalStyle`
   body {
